33da247bead941fdc60f27e0b9a6f44cb886bf84
Jason Breedlove <jason@jasonbreedlove.com>
2022-04-20 11:57:33 -0700

    switching active player

--- script.js
+++ script.js
@@ -1,37 +1,44 @@
 'use strict';
 
 // selecting the elements
+const player0Element= document.querySelector('.player--0');
+const player1Element= document.querySelector('.player--1');
 const player1ScoreElement = document.getElementById('score--0');
 const player2ScoreElement = document.getElementById('score--1');
+const currentScoreElement0 = document.getElementById('current--0');
+const currentScoreElement1 = document.getElementById('current--1');
 const diceElement = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const buttonHold = document.querySelector('.btn--hold');
 
 // starting conditions
 player1ScoreElement.textContent = 0;
 player2ScoreElement.textContent = 0;
 diceElement.classList.add('hidden')
 
+const scores = [0, 0];
+let currentScore = 0;
+let activePlayer = 0;
 // user rolls dice
-// class dice
-//generate random dice roll
-let diceRoll = Math.trunc(Math.random() * 6) + 1;
-console.log(diceRoll);
-// show dice roll
-let showDice = function(diceRoll){
-    switch(diceRoll){
-        case 1:
-            return 'dice-1.png';
-        case 2:
-            return 'dice-2.png';
-        case 3:
-            return 'dice-3.png';
-        case 4:
-            return 'dice-4.png';
-        case 5:
-            return 'dice-5.png';
-        case 6:
-            return 'dice-6.png';
-    }
-};
+btnRoll.addEventListener('click', function() {
+    let diceRoll = Math.trunc(Math.random() * 6) + 1;
+    diceElement.classList.remove('hidden');
+    diceElement.src = `dice-${diceRoll}.png`;
+    if (diceRoll !== 1){
+        currentScore += diceRoll;
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+    }else{
+
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        activePlayer = activePlayer === 0 ? 1 : 0;
+        currentScore = 0;
+        player0Element.classList.toggle('player--active');
+        player1Element.classList.toggle('player--active');
+        }
+
+});
+
 // check if dice is a 1
 
 // add dice roll to current score
